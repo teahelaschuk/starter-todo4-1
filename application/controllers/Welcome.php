4ad463039511346ec80b9d6db9af08044377e9ba
Nathan <Getryfp@gmail.com>
2017-10-12 16:08:14 -0700

    updated controller welcome, core My_controller and view homepage to allow for tasks to be seen and listed.

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -19,7 +19,27 @@ class Welcome extends Application
 	 */
 	public function index()
 	{
+
 		$this->data['pagebody'] = 'homepage';
+        $tasks = $this->tasks->all();   // get all the tasks
+
+// count how many are not done
+        $count = 0;
+        foreach($tasks as $task) {
+            if ($task->status != 2) $count++;
+        }
+// and save that as a view parameter
+        $this->data['remaining_tasks'] = $count;
+        $count = 0;
+        //Creates a list of priority
+        foreach(array_reverse($tasks) as $task) {
+            //shows priority code text
+            $task->priority = $this->app->priority($task->priority);
+            $display_tasks[] = (array) $task;
+            $count++;
+            if ($count >= 5) break;
+        }
+        $this->data['display_tasks'] = $display_tasks;
 		$this->render(); 
 	}
 
